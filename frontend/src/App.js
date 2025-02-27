f6297c43c841f074788985d9411cf102641d4553
Ranjan Kumar Chanda <ranjanchanda06@gmail.com>
2025-02-27 16:44:45 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import Home from "./Home";
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <>
     <h1 className="bg-gray-900 text-white h-screen p-2">
-      <Home/>
+    <Routes>
+    <Route exact path="/" element={<Home/>} />
+    
+    </Routes>
+      
     </h1>
     </>
   );
